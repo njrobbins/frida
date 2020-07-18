7f79296bbbabcc5024cbc9170d8ec6ef6c52d6e9
Jonah Falk <jaf12@live.missouristate.edu>
2020-07-18 12:38:50 -0500

    Test for how many frames detect a person.

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ camera.set(10, videoBrightness)
 time.sleep(1)  # Gives the camera's auto-focus & auto-saturation time to load
 fgbg = cv2.createBackgroundSubtractorMOG2()  # Initiates a background subtraction/foreground detection process
 frameCount = 0  # Used to delay fall detection to prevent false positives
+detectionTest = 0 # Used to keep track of successful detection
+personDetected = 0 # Track amount of frames detected
+percentageDetected = 0 # Percentage of frames person is detected
 
 if not camera.isOpened():
     raise IOError("CANNOT OPEN WEBCAM")
@@ -23,7 +26,8 @@ while True:
     try:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         fgmask = fgbg.apply(gray)
-        _, contours, _ = cv2.findContours(fgmask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)  # Find contours
+        contours, hierarchy = cv2.findContours(fgmask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)  # Find contours
+        # _, contours, _ = cv2.findContours(fgmask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)  # Use with OpenCvVersion < 4
 
         if contours:
             areas = []  # A list to hold all the areas
@@ -44,6 +48,7 @@ while True:
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255),
                               2)  # Puts red rectangle around fallen object
                 frameCount += 1
+                personDetected += 1
 
             if frameCount > 75:  # After ~2 seconds (at 30 fps) of being fallen down; will need to be optimized
                 print("FALL DETECTED")  # Prints this every time a fall is detected
@@ -52,14 +57,19 @@ while True:
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0),
                               2)  # Puts green rectangle around detected object
                 frameCount = 0
+                personDetected += 1
 
             cv2.imshow("Video Feed", frame)  # Loads video feed window
 
+            detectionTest += 1
+
             key = cv2.waitKey(1) & 0xFF
             if key == ord('q'):  # Press 'q' to terminate video feed
+                percentageDetected = personDetected / detectionTest
+                print(percentageDetected * 100, "% of frames detected person")
                 print("VIDEO FEED TERMINATED")
                 camera.release()
                 cv2.destroyAllWindows()
 
-    except:  # Can be edited
+    except:
         break
