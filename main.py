1a4966e1f4ed10db27b2122ac54c4c3c34e7bebb
Jonah Falk <jaf12@live.missouristate.edu>
2020-07-26 13:11:40 -0500

    Make system print when target is moving or not moving after 1 second of tracking.

--- main.py
+++ main.py
@@ -14,9 +14,11 @@ camera.set(10, videoBrightness)
 time.sleep(1)  # Gives the camera's auto-focus & auto-saturation time to load
 fgbg = cv2.createBackgroundSubtractorMOG2()  # Initiates a background subtraction/foreground detection process
 frameCount = 0  # Used to delay fall detection to prevent false positives
-detectionTest = 0 # Used to keep track of successful detection
-personDetected = 0 # Track amount of frames detected
-percentageDetected = 0 # Percentage of frames person is detected
+detectionTest = 0  # Used to keep track of successful detection
+personDetected = 0  # Track amount of frames detected
+percentageDetected = 0  # Percentage of frames person is detected
+movementCount = 0  # Keeps count of frames for movement updates
+movementState = "Not Moving"  # Starts out assuming person is standing still
 
 if not camera.isOpened():
     raise IOError("CANNOT OPEN WEBCAM")
@@ -54,10 +56,24 @@ while True:
                 print("FALL DETECTED")  # Prints this every time a fall is detected
 
             if h > w:
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0),
+                start_point = (x, y)  # Gives reference for where person starts
+                cv2.rectangle(frame, start_point, (x + w, y + h), (0, 255, 0),
                               2)  # Puts green rectangle around detected object
+
+                if movementCount == 0:  # Sets checkPoint to startPoint to keep track of original location
+                    checkPoint = start_point
                 frameCount = 0
                 personDetected += 1
+                movementCount += 1  # Increase MovementCount so it can be tracked each second
+
+                if movementCount > 30 and checkPoint == start_point:
+                    movementState = "No Movement"  # Updates state to No Movement if target does not move for one second
+                    movementCount = 0
+
+                if movementCount > 30 and checkPoint != start_point:
+                    movementState = "Moving"  # Updates state to Moving if target moves during the second
+                    movementCount = 0
+
 
             cv2.imshow("Video Feed", frame)  # Loads video feed window
 
@@ -68,8 +84,10 @@ while True:
                 percentageDetected = personDetected / detectionTest
                 print(percentageDetected * 100, "% of frames detected person")
                 print("VIDEO FEED TERMINATED")
+                print("Movement State: ", movementState)
                 camera.release()
                 cv2.destroyAllWindows()
 
-    except:
+    except Exception as e:
+        print (e)
         break
