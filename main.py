5fe00ca2c4f578463db497bb0dc0b271b7c72592
Niko Robbins <njrobbins@protonmail.com>
2020-07-27 22:09:59 -0500

    reorganized code

--- main.py
+++ main.py
@@ -27,6 +27,68 @@ import threading
 # print("torchvision version:", torchvision.__version__)
 
 
+# (Option 1) Use this for live video feed via a webcam.
+# Press 'q' to terminate.
+class CameraSetUpLiveVideo:
+    def __init__(self, port=-1):
+        self.cameraPort = port
+        self.camera = cv2.VideoCapture(self.cameraPort)
+        self.camera.set(cv2.CAP_PROP_FPS, 32)  # Sets frames per second (FPS).
+        video_brightness = 150
+        self.camera.set(10, video_brightness)
+        time.sleep(1)  # Gives the camera's auto-focus & auto-saturation time to load.
+
+
+# (Option 2) Use this for video file playback.
+# Video files will terminate once finished.
+class CameraSetUpVideoPlayBack:
+    def __init__(self, path_to_video):
+        self.path_to_video = path_to_video
+        self.camera = cv2.VideoCapture(self.path_to_video)
+        self.camera.set(cv2.CAP_PROP_FPS, 32)  # Set frames per second (FPS).
+        video_brightness = 150
+        self.camera.set(10, video_brightness)
+        time.sleep(1)  # Gives the camera's auto-focus & auto-saturation time to load.
+
+
+class CreateBatch:
+    def __init__(self):
+        self.batch_size = 32
+        self.batch = []
+        self.condense_batch = []
+
+
+class LoadModel:
+    def __init__(self):
+        self.onnx_model = onnx.load('model.onnx')
+        self.model = cv2.dnn.readNetFromONNX('model.onnx')
+        self.sess = ort.InferenceSession('model.onnx')
+        self.input_name = self.sess.get_inputs()[0].name
+
+
+class MotionHistoryDifference:
+    def __init__(self, frame):
+        self.frame = frame
+        self.resized = cv2.resize(self.frame, MotionHistoryTransform.dim, interpolation=cv2.INTER_AREA)
+
+
+class MotionHistoryTransform:
+    dim = (224, 256)
+
+    def __init__(self, frame):
+        self.frame = frame
+        self.dims = (256, 224, 3)
+        self.dim = (224, 256)
+        self.mhi_zeros = np.zeros(self.dims)
+        self.prev_frame = cv2.resize(self.frame, self.dim, interpolation=cv2.INTER_AREA)
+
+
+class TransformShape:
+    def __init__(self, frame):
+        self.frame = frame
+        self.frame_transform = np.zeros_like(self.frame)
+
+
 def animate():
     for c in itertools.cycle(['|', '/', '-', '\\']):
         if done:
@@ -40,139 +102,128 @@ def animate():
 done = False  # Program hasn't yet loaded video.
 t = threading.Thread(target=animate)
 t.start()
+model = LoadModel()
+batchCreate = CreateBatch()
 
-# Only change cameraPort for debugging purposes.
-cameraPort = 0  # 0 = system's default webcam (recommended), 1 = external webcam, -1 = auto-detection
-# Option 1: Use this for live video feed via a webcam. Press 'q' to terminate.
-camera = cv2.VideoCapture(cameraPort)
-# Option 2: Use this for a video file. Refer to the adl, fallcam0, & fallcam1 dataset folders.
-# The videos are short and will terminate once finished.
-# Make sure to comment out option 1 for VideoCapture (above) if used.
-# camera = cv2.VideoCapture('fallcam1/fall30cam1.mp4')
+# (Option 1) Use this class for live video feed.
+# 0 = system's default webcam (recommended), 1 = external webcam, -1 = auto-detection
+# Only change argument for debugging purposes.
+# camera = CameraSetUpLiveVideo(0)
 
-camera.set(cv2.CAP_PROP_FPS, 30)  # Sets frames per second (FPS).
-videoBrightness = 150
-camera.set(10, videoBrightness)
-time.sleep(1)  # Gives the camera's auto-focus & auto-saturation time to load.
+# (Option 2) Use this class for video file playback.
+# Change "fallcam0/fall1cam0.mp4" to the video file of your choice.
+# Refer to the adl, fallcam0, & fallcam1 dataset folders.
+camera = CameraSetUpVideoPlayBack("fallcam0/fall1cam0.mp4")
 
-# Explain this group
-onnxModel = onnx.load('model.onnx')
-model = cv2.dnn.readNetFromONNX('model.onnx')
-sess = ort.InferenceSession('model.onnx')
-
-batch = []
-batchSize = 32
 color = np.random.randint(0, 255, (100, 3))
-condenseBatch = []
 countFrame = 0
-HUD = 1  # Heads Up Display. Set to 0 to turn off.
-inputName = sess.get_inputs()[0].name
-prevFrame = None
+HUD = 1  # Heads Up Display for live video feed. Set to 0 to turn off.
+mhi_maker = None
 prev_mhi = None
 
-if not camera.isOpened():
+if not camera.camera.isOpened():
     raise IOError("CANNOT OPEN WEBCAM")
 
 while True:
-    grabbed, frame = camera.read()
+    grabbed, frame = camera.camera.read()
     if not grabbed:
         break
 
     try:
         countFrame += 1
-        frame_x = frame
-        img2 = np.zeros_like(frame)
+        frameTransform = TransformShape(frame)
         frame = cv2.cvtColor(np.array(frame), cv2.COLOR_BGR2GRAY)
-        img2[:, :, 0] = frame
-        img2[:, :, 1] = frame
-        img2[:, :, 2] = frame
-        frame = img2
-        dim = (224, 256)
-        dims = (256, 224, 3)
-        mhi_zeros = np.zeros(dims)
-        detectStatus = "Idle"
+        frameTransform.frame_transform[:, :, 0] = frame
+        frameTransform.frame_transform[:, :, 1] = frame
+        frameTransform.frame_transform[:, :, 2] = frame
+        frame = frameTransform.frame_transform
 
         if countFrame == 1:
-            prevFrame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
-            prev_mhi = mhi_zeros
+            mhi_maker = MotionHistoryTransform(frame)
+            prev_mhi = mhi_maker.mhi_zeros
 
         else:
-            resized = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
-            diff = cv2.absdiff(prevFrame, resized)
+            mhi_difference_maker = MotionHistoryDifference(frame)
+            diff = cv2.absdiff(mhi_maker.prev_frame, mhi_difference_maker.resized)
             binary = (diff >= (.41 * 255)).astype(np.uint8)
-            mhi = binary + (binary == 0) * np.maximum(mhi_zeros, (prev_mhi - 1 / 16))
-            prev_frame = resized
+            mhi = binary + (binary == 0) * np.maximum(mhi_maker.mhi_zeros, (prev_mhi - 1 / 16))
+            mhi_maker.prev_frame = mhi_difference_maker.resized
             prev_mhi = mhi
-            img2 = mhi
+            frameTransform.frame_transform = mhi
 
-        img2 = cv2.resize(img2, dim, interpolation=cv2.INTER_AREA)
-        frames = np.expand_dims(img2, axis=0)
+        frameTransform.frame_transform = cv2.resize(frameTransform.frame_transform,
+                                                    MotionHistoryTransform.dim, interpolation=cv2.INTER_AREA)
+        frames = np.expand_dims(frameTransform.frame_transform, axis=0)
         frames = np.array(frames)
         frames = frames.astype(numpy.float32)
         image = torch.from_numpy(frames)
         image = image.permute(0, 3, 1, 2)
+        detectStatus = "Idle"
 
-        # This is for the append current frame to next frame
-        # if(countFrame == 1):
-        # result = np.array(image)
-        # elif(countFrame > 1 and countFrame %2 == 0):
-        # result = np.array(image)
-        # condense = np.array(image)'''
+        if countFrame == 1:
+            result = np.array(image)
 
-        # This is for normal use current frame then empty and take next frame
+        elif countFrame > 1 and countFrame % 2 == 0:
+            result = np.array(image)
+
+        batchCreate.condense = np.array(image)
         result = np.array(image)
 
-        if len(batch) != 32:
-            batch.append(result)
-            # (Alternative) Only used for condensed-spaced model:
-            # condenseBatch.append(condense)
+        if len(batchCreate.batch) != 32:
+            # not sure what's going on here
+            batchCreate.batch.append(result)
+            # Uncomment only for condensed-spaced model if needed:
+            # condense_batch.append(condense)
 
-        if len(batch) > 32:
-            batch = batch[:32]
-            batch = np.array(batch)
+        if len(batchCreate.batch) > 32:
+            batchCreate.batch = batchCreate.batch[:32]
+            batchCreate.batch = np.array(batchCreate.batch)
 
-        if len(batch) == 32:
-            result_x = np.concatenate(batch, axis=0)
-            x = result_x
-            res = sess.run(None, {inputName: x})
+        if len(batchCreate.batch) == 32:
+            result_x = np.concatenate(batchCreate.batch, axis=0)
+            model_input_x = result_x
+            res = model.sess.run(None, {model.input_name: model_input_x})
             norm = softmax(res[0])
 
             for x in norm:
                 fall = x.item(0)
                 notFall = x.item(1)
-                # FP = Fall Prediction, NFP = Non-Fall Prediction
-                print("FP", "{0:.2%}".format(fall),
-                      "NFP", "{0:.2%}".format(notFall))
+                print("FP", "{0:.2%}".format(fall), "NFP", "{0:.2%}".format(notFall))
                 if fall > notFall:
                     detectStatus = "FALL DETECTED"
                     print(detectStatus)
-                    batch = []
+                    batchCreate.batch = []
+                    break
 
-            # (Default) Used for the regular current frame, then next frame is its own frame also:
-            batch = []
-            # (Alternative) Used to test the appending of the current frame to the next frame:
-            # batch = condenseBatch[16::]
-            # condenseBatch = []
+            # tidy this please
+            # here we are testing Condensed-Spaced Model by appending of current frame to next frame
+            batchCreate.batch = batchCreate.condense_batch[16::]
+            batchCreate.condense_batch = []
+
+            # also this
+            # here we are doing just testing the Regular Model current frame
+            # and then next frame never existing in same batch
+            # batchCreate.batch = []
 
         if HUD:
             if detectStatus == "FALL DETECTED":
-                cv2.putText(frame, "Status: {}".format(detectStatus), (10, 20),
-                            cv2.FONT_HERSHEY_DUPLEX, 0.8, (0, 0, 255), 1)
+                cv2.putText(frameTransform.frame_transform, "Status: {}".format(detectStatus),
+                            (10, 20), cv2.FONT_HERSHEY_DUPLEX, 0.8, (0, 0, 255), 1)
             else:
-                cv2.putText(frame, "Status: {}".format(detectStatus), (10, 20),
-                            cv2.FONT_HERSHEY_DUPLEX, 0.8, (0, 128, 0), 1)
+                cv2.putText(frameTransform.frame_transform, "Status: {}".format(detectStatus),
+                            (10, 20), cv2.FONT_HERSHEY_DUPLEX, 0.8, (0, 128, 0), 1)
 
         # (Default) Loads video frame window in grayscale:
         cv2.imshow("Video Feed", frame)
         # (Optional) Loads video frame window using background subtraction:
-        # cv2.imshow("Background Subtraction", img2)
+        # cv2.imshow("Background Subtraction", frameTransform.frame_transform)
 
         done = True  # Video has successfully loaded.
 
         key = cv2.waitKey(1) & 0xFF
         if key == ord('q'):
             print("\nVIDEO FEED TERMINATED\n")
-            camera.release()
+            camera.camera.release()
             cv2.destroyAllWindows()
             break
 
